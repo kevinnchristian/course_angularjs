f87bcc9037abe798979c6c3bc61ad13d6c023940
kevinnchristian <kevin.cmribeiro@outlook.com>
2021-01-07 15:09:43 -0300

    :construction: Filters angularjs

---
+++ filters/annotations.js
@@ -0,0 +1,34 @@
+// ---------- Filters ---------- //
+// -> Obs.: Os FILTROS nem sempre FILTRAM!
+// -> Transformam o resultado de uma expressão, relizando operações como a formatação
+//    de data, a conversão de moeda e a ordenação de Array
+// -> Podemos aplicar filters no Controllers e Views
+//    View os filters sempre vai ser reavaliados e vai estar sendo executado de novo e de novo
+//    Controller é a forma mais perfomatica
+//    Controller para aplicar temos que injetar a depedência $filter ou pode ser
+//    desta outra forma ex: uppercaseFilter, lowercaseFilter
+
+// -> uppercase
+//    Transforma uma STRING em letra maiúscula
+
+// -> lowercase
+//    Transforma uma STRING em letra minúscula
+
+// -> date
+//    Formata uma data usando uma máscara
+
+// -> filter
+//    Filtra um Array com base em um critério
+
+// -> orderBy
+//    Ordena um Array com base em um critério
+
+// -> currency
+//    Converte um número para moeda
+
+// -> number
+//    Formata um número
+
+// -> limitTo
+//    Limita um Array ou uma String
+
