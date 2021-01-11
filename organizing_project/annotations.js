67cab99aae820c09fe17690fdc68ecaac192e20b
kevinnchristian <kevin.cmribeiro@outlook.com>
2021-01-11 15:22:23 -0300

    :art: Organizing project angularjs

---
+++ organizing_project/annotations.js
@@ -0,0 +1,13 @@
+// ---------- Organizing Project ---------- //
+
+// -> Inline Style - Tudo no mesmo arquivo
+//    Utilizado em projetos simples que tenha poucas coisas
+
+// -> Stereotyped Style - Componentes do mesmo tipo juntos
+//    Utilizado em projetos pequenos
+
+// -> Specific Style - Um arquivo para cada componente
+//    Utilizado em projetos médios
+
+// -> Domain Style - Agrupando os arquivos por domínio/scopo
+//    Utilizado em projetos grandes
