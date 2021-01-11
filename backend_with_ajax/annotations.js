f1456a15a86c3de7d81312f90be5869c2bd9f6af
kevinnchristian <kevin.cmribeiro@outlook.com>
2021-01-11 13:37:51 -0300

    :construction: Backend integration angularjs

---
+++ backend_with_ajax/annotations.js
@@ -0,0 +1,22 @@
+// ---------- Back-end integration ---------- //
+
+// -> $http
+//    O serviço $http do Angular permite a realização de requisições utilizando
+//    XMLHttpRequest ou via JSONP:
+//        * get (url, config)
+//        * post (url, data, config)
+//        * put (url, data, config)
+//        * delete (url, config)
+//        * head (url, config)
+//        * jsonp (url, config)
+
+// -> SOP - Same-Origin Policy
+//    Política de restrição de segurança que impede que o navegador acesse recursos
+//    alheios a sua origem, considerando protocolo, host e porta
+
+// -> JSONP - JSON with padding
+//    Estratégia utilizada para burlar o mecanismo de proteção dos navegadores em 
+//    relação ao acesso de recursos externos
+
+// -> CORS - Cross-Origin Resource Sharing
+//    Permissão para acessar recursos externos por meio de cabeçalhos HTTP adicionais
