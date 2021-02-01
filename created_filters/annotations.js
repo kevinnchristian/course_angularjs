00a8e50cc17d8200b26a3185ca5f856352725d06
kevinnchristian <kevin.cmribeiro@outlook.com>
2021-02-01 10:58:40 -0300

    :construction: Add folder created_filters

---
+++ created_filters/annotations.js
@@ -0,0 +1,6 @@
+// ---------- Created Filters ---------- //
+
+// -> Filters don't always filter
+//    Transformam o resultados de uma expressão, realizando operações como a formatação
+//    de data,a conversção de moeda e a ordenação de Array.
+
