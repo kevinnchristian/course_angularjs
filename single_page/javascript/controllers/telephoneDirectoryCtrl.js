956b6ae5086906c0282781d4820665c8c6810ce8
kevinnchristian <kevin.cmribeiro@outlook.com>
2021-02-10 18:32:22 -0300

    :construction: Add folder single_page

---
+++ single_page/javascript/controllers/telephoneDirectoryCtrl.js
@@ -0,0 +1,55 @@
+angular.module('telephoneDirectory').controller('telephoneDirectoryCtrl', function ($scope, $filter, $http, serialGenerator) {
+  $scope.app = 'Telephone Directory';
+  $scope.contact = {
+    date: 895892400000
+  };
+  var loadingContacts = function () {
+    $http.get('http://localhost:3412/contacts').then(function (res) {
+      res.data.forEach(function (item) {
+        item.serial = serialGenerator.generate();
+      });
+      $scope.contacts = res.data;
+    }, function (res) {
+      $scope.message = 'Não foi possível carregar os dados.'
+    })
+  };
+  var loadingOperators = function () {
+    $http.get('http://localhost:3412/operators').then(function (res) {
+      $scope.operators = res.data;
+    })
+  };
+  var loadingColors = function () {
+    $http.get('http://localhost:3412/colors').then(function (res) {
+      $scope.colors = res.data;
+    })
+  };
+  $scope.addContact = function (contact) {
+    contact.serial = serialGenerator.generate();
+    contact.date = new Date();
+    $http.post('http://localhost:3412/contacts', contact).then(function (res) {
+      delete $scope.contact;
+      $scope.contactForm.$setPristine();
+      loadingContacts();
+    })
+  };
+  $scope.deleteContact = function (contacts) {
+    $scope.contacts = contacts.filter(function (contact) {
+      if (!contact.selected) return contact;
+    });
+  };
+  $scope.isContactSelected = function (contacts) {
+    if (contacts != undefined) {
+      return contacts.some(function (contact) {
+        return contact.selected;
+      });
+    }
+  };
+  $scope.orderByField = function (field) {
+    $scope.criteriaOrder = field;
+    $scope.directionOrder = !$scope.directionOrder;
+  };
+
+  loadingContacts();
+  loadingOperators();
+  loadingColors();
+});
